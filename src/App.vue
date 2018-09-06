0d1b87a9d8459a2d0c86c7443604be54b73d5ada
mivkovic <markovicm9000@gmail.com>
2018-09-06 21:58:09 +0200

    list of repos

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,27 @@
         </v-ons-button>
       </template>
     </app-toolbar>
+
     <div class="container">
+
       <app-search :query.sync="query" />
-      <div v-for="repo in repos" :key="repo.id">
-        {{ repo.name }}
-      </div>
-    </div>
 
+      <v-ons-list-header class="header">
+        Repositories of {{ this.query }}
+      </v-ons-list-header>
+
+      <v-ons-list v-if="repos" v-for="repo in repos" :key="repo.id" >
+        <v-ons-list-item>
+            <div class="left">
+              <img class="list-item__thumbnail" :src="repo.owner.avatar_url">
+              
+            </div>
+            <span class="list-item__title">{{ repo.name }}</span>
+            <span class="list-item__subtitle">{{ repo.description }}</span>
+        </v-ons-list-item>
+      </v-ons-list>
     
-
+    </div>
 
   </v-ons-page>
 </template>
@@ -50,6 +62,7 @@ export default {
           this.repos = response.data
           console.log(this.repos)
         })
+        .catch(err => console.log(err))
     }, 500)
     
   }
@@ -63,6 +76,10 @@ export default {
   margin: 0 auto;
   width: 80%;
   padding: 10px;
+}
+
+.header {
+  margin: 10px;
 }
 
 </style>
