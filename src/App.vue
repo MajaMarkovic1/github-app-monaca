bf2ba607379f7dd95ba1f60eac9781054a0a0824
mivkovic <markovicm9000@gmail.com>
2018-09-07 21:26:14 +0200

    error 404

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,20 @@
       <app-search :query.sync="query" />
 
       <v-ons-list >
+
         <v-ons-list-header class="header">
           Repositories of {{ this.query }}
         </v-ons-list-header>
+
         <p>
           <v-ons-progress-circular indeterminate v-if="loading"></v-ons-progress-circular>
         </p>
-        <empty-state v-if="repos.length <= 0 && query !== ''" :type="'repo'"/>
+
+        <empty-state v-if="showEmptyState" :type="'repo'"></empty-state>
+
+        <error404 v-if="show404" />
         
-      
-        <v-ons-list-item v-if="repos" v-for="repo in repos" :key="repo.id">
+        <v-ons-list-item v-if="showList" v-for="repo in repos" :key="repo.id">
             <div class="left">
               <img class="list-item__thumbnail" :src="repo.owner.avatar_url">
             </div>
@@ -43,6 +47,7 @@ import AppToolbar from './components/AppToolbar'
 import AppSearch from './components/AppSearch'
 import { githubService } from './services/Github'
 import EmptyState from './components/EmptyState'
+import Error404 from './components/Error404'
 
 
 export default {
@@ -50,7 +55,8 @@ export default {
   components: {
     AppToolbar,
     AppSearch,
-    EmptyState
+    EmptyState,
+    Error404
   },
 
   data() {
@@ -58,7 +64,20 @@ export default {
       query: '',
       repos: [],
       error: '',
-      loading: false
+      notFound: false,
+      loading: false,
+    }
+  },
+
+  computed: {
+    showEmptyState () {
+      return this.query && !this.notFound && !this.loading && !this.repos.length
+    },
+    show404 () {
+      return this.notFound && !this.isFetching
+    },
+    showList () {
+      return this.query && !this.notFound && !this.loading && this.repos.length
     }
   },
 
@@ -67,10 +86,13 @@ export default {
       this.loading = true
       githubService.getRepos(newValue)
         .then((response) => {
-          this.repos = response.data
-          //console.log(this.repos)
+          this.repos = response.data 
+          console.log(this.repos)
         })
-        .catch(err => console.log(err))
+        .catch((err) => {
+          this.notFound = true
+          console.log(this.error = err.response.status)
+        })
         .finally(() => {
           this.loading = false
         })
